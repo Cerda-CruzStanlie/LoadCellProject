b3c75902cf2f5d960bb0ea5a476b82c66d4ba1ad
Stan <stanliecerda3@gmail.com>
2025-11-11 02:53:16 -0500

    Calibration File change, update, and recallibration

--- LoadCellCalibration.py
+++ LoadCellCalibration.py
@@ -1,24 +1,27 @@
 import serial
 import pandas as pd
 import os
-
+import numpy as np
 # Function to convert pounds to grams
 def pounds_to_grams(pounds):
     return pounds * 453.592
 
-# Configure serial
-ser = serial.Serial(port='/dev/ttyS0', baudrate=115200, timeout=1)
-
 # CSV file
-filename = 'readings.csv'
+filename = 'CalibrationReadings.csv'
 
 # Convert a number to a string and use it as the weight_label
-weight_in_grams = 0
+
+grams_inp = float(input('Grams:\t'))
+pound_inp = pounds_to_grams(float(input('Pounds:\t')))
+weight_in_grams = pound_inp + grams_inp
 weight_label = f"{weight_in_grams:.2f}"  # Convert to grams and format as a string
 col = str(weight_label)
 
-values = []
+# Configure serial
+ser = serial.Serial(port='/dev/ttyS0', baudrate=115200, timeout=1)
 
+values = []
+n = 100 #length
 try:
     # Load existing CSV or create a new one
     if os.path.exists(filename):
@@ -34,18 +37,17 @@ try:
     if col not in df.columns: 
         df[col] = pd.Series(dtype='float')
     else:  # Clear any existing values in that column
-        df.loc[:, col] = None
+        df.loc[:, col] = np.nan
     print('Data collection begining')
-    while True:
+    while len(values) < n:
         line = ser.readline()
         if line:
             val = float(line.decode(errors='ignore').strip())
             print(val)
             values.append(val)
 
-            
-
 finally:
-    df.loc[:, col] = {col:values}
+    df[col] = pd.Series(values)
     df.to_csv(filename, index=False) # Save back to CSV
     ser.close()
+
